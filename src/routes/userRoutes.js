ec8172479e447947b26f7aad9dda69ecfbc17bf3
Adam Le Bay <adam.lb@hotmail.com>
2025-05-30 17:05:54 +0200

    user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const { getUserProfile, updateUserProfile } = require('../controllers/userController');
+const { getUser, getUserProfile, updateUserProfile } = require('../controllers/userController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// Route to get user profile
-router.get('/profile', verifyToken, getUserProfile);
+// Route to get a user by ID
+router.get('/:id', getUser);
 
-// Route to update user profile
-router.put('/profile', verifyToken, updateUserProfile);
+// Route to get the logged-in user's profile
+router.get('/me', verifyToken, getUserProfile);
+
+// Route to update the logged-in user's profile
+router.put('/me', verifyToken, updateUserProfile);
 
 module.exports = router;
